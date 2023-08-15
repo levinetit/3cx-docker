e79eb09f7e065f350fcd61048e1f0e800384a615
levinetit <levinetit@gmail.com>
2023-08-15 18:16:54 +0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VERSION=18.0.8.917
+USER=levinetit
+HOSTNAME=levinetit.3cx.ro
+
+docker run \
+        -d \
+		--privileged \
+        --name 3cx \
+        --hostname ${HOSTNAME} \
+		--security-opt apparmor=unconfined \
+		--network host \
+        --restart unless-stopped \
+        -v 3cx_backup:/srv/backup \
+        -v 3cx_recordings:/srv/recordings \
+        -v 3cx_log:/var/log \
+        --cap-add SYS_ADMIN \
+        --cap-add NET_ADMIN \
+        ${USER}/3cx:${VERSION}
